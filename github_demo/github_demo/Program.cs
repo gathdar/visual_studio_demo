7c3d9412ed7078e5e7085ebd6b46da54b171fd2a
Justin <theoperaking@yahoo.com>
2018-05-29 10:33:53 -0700

    made a test file

---
+++ github_demo/github_demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github demo");
+            Console.ReadLine();
+        }
+    }
+}
